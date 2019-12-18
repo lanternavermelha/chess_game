0726fd69791ddab2cb662f6573ff887b5de5f6f7
lanternavermelha <f_figueira@hotmail.com>
2019-12-18 16:35:43 +0000

    Created class Position

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(1,1);
+		
+		System.out.println(pos);
+
+	}
+
+}
