c201ece5102df5bba594101ad83c9657716cbecc
lanternavermelha <f_figueira@hotmail.com>
2019-12-20 18:07:20 +0000

    Special move promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,20 +25,26 @@ public class Program {
 				System.out.println();
 				System.out.print("\nSource: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
+
 				System.out.print("\nTarget: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
+
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
